e8e0563bc132ed64568fe85ac7c8a26d519700c8
coni.dev <ian.gorski18@gmail.com>
2025-10-14 22:18:01 -0300

    config: habilita carga de imágenes remotas para el icono de Google

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+    ],
+  },
   // Eliminar cualquier referencia a dotenv/config ya que no es necesario aquí
 };
 
